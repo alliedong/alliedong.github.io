b2bf381857e6625df8f9228ea7e3edc792f1ccf4
Allie Dong <allied9420@gmail.com>
2021-05-26 19:02:30 -0400

    Created organization files

---
+++ src/setupTests.js
@@ -0,0 +1,7 @@
+//Research this!!
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows users to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: http://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';

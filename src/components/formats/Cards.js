a1736b0ef9d6baafdf50c7ca66ec37a4599468f2
Allie Dong <allied9420@gmail.com>
2021-09-19 15:27:18 -0400

    Added pictures to cards

--- src/components/formats/Cards.js
+++ src/components/formats/Cards.js
@@ -10,7 +10,7 @@ function Cards(props) {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <SingleCard
-              src="images/splitpeas.jpg"
+              src="../../images/splitpeasprojectgraphic.png"
               text="A full stack grocery shopping mobile app targetted toward 
               university students to help them split trips and bills within a group. 
               SplitPeas: Save Food. Save Money. Save Time."
@@ -18,7 +18,7 @@ function Cards(props) {
               path="https://bit.ly/3Ctr8xQ"
             />
             <SingleCard
-              src="images/sportipaws.jpg"
+              src="../../images/sportipawsprojectgraphic.png"
               text="A specialized app to track physical activity during dog walks. Case study 
               completed with User Research, Competitive Analysis, information architecture and more
               to add in the creation of low and high fidelity prototypes."
@@ -28,20 +28,20 @@ function Cards(props) {
           </ul>
           <ul className="cards__items">
             <SingleCard
-              src="images/moneytree.jpg"
-              text="A gamefied, gardening-themed app to aid simple financial management, 
-                promote financial literacy, and raise awareness about the environment for children. 
-                Created to make personal finance more accessible, easy and engaging."
-              label="MoneyTree: UX Design, PM"
-              path="https://devpost.com/software/money-tree-personal-finance-management-for-all-youth"
-            />
-            <SingleCard
-              src="images/extendyourhorizons.jpg"
+              src="../../images/extendyourhorizonsprojectgraphic.png"
               text="A practical chrome extension consisting of four features: a Pomodoro timer,
                 a to-do list, a customizable website blocker, and an inspirational quote generator. 
                 Goal is to help students with productivity, etticiency, and mental wellness."
               label="Extend Your Horizons: Design, Development, PM"
               path="https://devpost.com/software/extend-your-horizons"
+            />
+            <SingleCard
+              src="../../images/moneytreeprojectgraphic.png"
+              text="A gamefied, gardening-themed app to aid simple financial management, 
+                promote financial literacy, and raise awareness about the environment for children. 
+                Created to make personal finance more accessible, easy and engaging."
+              label="MoneyTree: UX Design, PM"
+              path="https://devpost.com/software/money-tree-personal-finance-management-for-all-youth"
             />
           </ul>
         </div>
